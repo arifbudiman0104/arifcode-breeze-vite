74e48ef4dd15805b006904be88002ceb593afba5
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-23 07:33:08 +0700

    register with username

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
 
     <div class="pt-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
-            <div class="bg-white dark:bg-gray-900 overflow-hidden mb-6 shadow-sm sm:rounded-lg">
+            <div class="bg-white pb-5 dark:bg-gray-900 overflow-hidden mb-6 shadow-sm sm:rounded-lg">
                 {{-- <div class="mb-5 bg-white dark:bg-slate-700 overflow-hidden shadow-sm sm:rounded-lg">
                     <div
                         class="p-6 bg-white dark:bg-slate-700 dark:text-white border-b border-gray-200 dark:border-slate-600">
